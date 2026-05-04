77af92b080f5c2c041c632e4208668aba2f0e474
luhh_fvl <luana.fvlima@gmail.com>
2026-05-03 23:21:11 -0300

    feat: adicionar pagina inicial e extrair header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    SharedModule,
     RouterModule.forRoot([
-      { path: '', redirectTo: 'matches', pathMatch: 'full' },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
       { path: 'matches', loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule) },
       { path: 'common-components', loadChildren: () => import('./common-components/common-components.module').then(m => m.CommonComponentsModule) }
     ])
